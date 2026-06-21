47f19ef696728023db4f067cccc3e48b8246c236
Андрей Мохов <a.mokhov@i-teco.ru>
2026-06-21 20:47:25 +0300

    migration: add translations for speedrun choice

---
+++ migrations/009_speedrun_choice_translations.sql
@@ -0,0 +1,3 @@
+UPDATE public.choices
+SET translations = '{"en": {"label": "🥁 Play a solo part on the ventilation pipe"}, "es": {"label": "🥁 Toca una parte solista en el tubo de ventilación."}, "pt_br": {"label": "🥁 Toque um solo no tubo de ventilação"}, "de": {"label": "🥁 Spielen Sie einen Solopart am Lüftungsrohr"}}'::jsonb
+WHERE id = 'ch_1_hub_fast_death';
